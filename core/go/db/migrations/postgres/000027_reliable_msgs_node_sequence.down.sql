fc2831767fd27228ae12ffb65a623fb5f57f556f
Matthew Whitehead <matthew.whitehead@kaleido.io>
2026-03-24 17:42:02 +0000

    Use concurrent index building on the downgrade path as well
    Signed-off-by: Matthew Whitehead <matthew.whitehead@kaleido.io>

--- core/go/db/migrations/postgres/000027_reliable_msgs_node_sequence.down.sql
+++ core/go/db/migrations/postgres/000027_reliable_msgs_node_sequence.down.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-CREATE INDEX reliable_msgs_node ON reliable_msgs ("node");
+CREATE INDEX CONCURRENTLY reliable_msgs_node ON reliable_msgs ("node");
 DROP INDEX reliable_msgs_node_sequence;
 
 COMMIT;
