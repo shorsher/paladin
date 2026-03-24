789451ee2da9baf2f0774b5cbbd98edde34b05a0
Matthew Whitehead <matthew.whitehead@kaleido.io>
2026-03-24 17:33:49 +0000

    Remove comments - not necessary
    Signed-off-by: Matthew Whitehead <matthew.whitehead@kaleido.io>

--- core/go/db/migrations/postgres/000027_reliable_msgs_node_sequence.up.sql
+++ core/go/db/migrations/postgres/000027_reliable_msgs_node_sequence.up.sql
@@ -1,7 +1,5 @@
 BEGIN;
 
--- Supports reliable message drain: WHERE node = ? AND sequence > ? ORDER BY sequence ASC (peer.reliableMessageScan).
--- Leading column "node" still serves filters on node alone (e.g. API queries).
 CREATE INDEX reliable_msgs_node_sequence ON reliable_msgs ("node", "sequence");
 DROP INDEX reliable_msgs_node;
 
