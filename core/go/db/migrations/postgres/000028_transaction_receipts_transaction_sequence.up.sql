237db0f7e15683af735ef499bb4881ce0183705d
Matthew Whitehead <matthew.whitehead@kaleido.io>
2026-03-25 13:56:47 +0000

    Create the new index concurrently
    Signed-off-by: Matthew Whitehead <matthew.whitehead@kaleido.io>

--- core/go/db/migrations/postgres/000028_transaction_receipts_transaction_sequence.up.sql
+++ core/go/db/migrations/postgres/000028_transaction_receipts_transaction_sequence.up.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
 -- Keeps existing UNIQUE ("transaction") index from migration 000015_transaction_receipt_listeners
-CREATE INDEX transaction_receipts_transaction_sequence ON transaction_receipts ("transaction", "sequence" DESC);
+CREATE INDEX CONCURRENTLY transaction_receipts_transaction_sequence ON transaction_receipts ("transaction", "sequence" DESC);
 
 COMMIT;
